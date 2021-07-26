0fd38a63c54d48e168a5cae0e2746bc1850919f3
Leoš Karásek <leos.karasek@seznam.cz>
2021-07-26 13:49:23 +0200

    hrani si s polohou

--- app/src/main/java/com/example/mhd/MainActivity.java
+++ app/src/main/java/com/example/mhd/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.mhd;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.app.VoiceInteractor;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -15,11 +20,16 @@ import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Calendar;
+
+
+public class MainActivity extends AppCompatActivity implements LocationListener {
 
     private EditText edA;
     private EditText edB;
     private LocationManager locationManager;
+    public Criteria criteria;
+    public String bestProvider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,11 +59,67 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+
     public void najdiPolohu(View v) {
 
-        //  LocationListener locationListener = new MyLocationListener();
+        //     LocationListener locationListener = new LocationListener();
+
+        ///////
+        criteria = new Criteria();
+        bestProvider = String.valueOf(locationManager.getBestProvider(criteria, true));
 
 
+        int REQUEST_CODE = 0;
+        int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
+
+        if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    REQUEST_CODE);
+        } else {
+            Location poloha = locationManager.getLastKnownLocation(bestProvider);
+
+            //This is what you need:
+            if (poloha != null) {
+                Log.e("TAG", "GPS is on");
+                double sirka = poloha.getLatitude();
+                double delka = poloha.getLongitude();
+                Log.d("Poloha Delka", String.valueOf(delka));
+                Log.d("Poloha Sirka", String.valueOf(sirka));
+                Log.d("Konec", String.format("Konec"));
+                // Toast.makeText(MainActivity.this, "latitude:" + latitude + " longitude:" + longitude, Toast.LENGTH_SHORT).show();
+                //  searchNearestPlace(voice2text);
+            } else {
+                locationManager.requestLocationUpdates(bestProvider, 1000, 0.1f, this);
+                Log.e("TAG", "GPS is on");
+                double sirka = poloha.getLatitude();
+                double delka = poloha.getLongitude();
+                Log.d("Poloha Delka", String.valueOf(delka));
+                Log.d("Poloha Sirka", String.valueOf(sirka));
+                Log.d("Konec", String.format("Konec"));
+            }
+
+
+            //  Location poloha = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+            //   double sirka = poloha.getLatitude();
+            //   double delka = poloha.getLongitude();
+
+
+        }
+        ///////
+
+
+        //   Intent intentP = new Intent(this, Vysledek.class);
+        //    startActivity(intentP);
+
+        //todo
+
+
+    }
+
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             // TODO: Consider calling
             //    ActivityCompat#requestPermissions
@@ -62,23 +128,17 @@ public class MainActivity extends AppCompatActivity {
             //                                          int[] grantResults)
             // to handle the case where the user grants the permission. See the documentation
             // for ActivityCompat#requestPermissions for more details.
-            Location poloha = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+        //    return;
+
+            locationManager.requestLocationUpdates(bestProvider, 1000, 0.1f, this);
+            Location    poloha = locationManager.getLastKnownLocation(bestProvider);
+            Log.e("TAG", "GPS is on");
             double sirka = poloha.getLatitude();
             double delka = poloha.getLongitude();
-
             Log.d("Poloha Delka", String.valueOf(delka));
-            Log.d("Poloha Sirka",String.valueOf(sirka));
-        //    return;
+            Log.d("Poloha Sirka", String.valueOf(sirka));
+            Log.d("Konec", String.format("Konec"));
         }
-
-
-
-
-
-     //   Intent intentP = new Intent(this, Vysledek.class);
-    //    startActivity(intentP);
-
-        //todo
 
 
     }
